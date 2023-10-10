c6ecbedf1922cb755fea02c86763d66a5f2fcf82
Srcox100 <lcox.esc@gmail.com>
2023-10-09 20:17:22 -0500

    001

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Login() {
     return (
-        <div>Hola, soy una sección del proyecto</div>
+        <body>
+        <div>Hola, soy una sección del proyecto GAAAA</div>
+        </body>
     )
 }
 
